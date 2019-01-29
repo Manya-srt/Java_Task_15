2a76dc8c4dcbeeff0f80cbfb08838036940c1c02
Mariya27 <mariya.akulova.91@inbox.ru>
2019-01-30 01:00:47 +0600

    Initial commit

---
+++ src/Task_15.java
@@ -0,0 +1,22 @@
+// Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt
+import java.io.*;
+
+public class Task_15{
+        public static void main(String[] args) {
+            String line;
+            try {
+                File file = new File("file.txt");
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                while((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                bufferedReader.close();
+            }
+            catch (IOException e) {
+                System.out.println("Ошибка ввода-вывода: " + e);
+            }
+        }
+    }
